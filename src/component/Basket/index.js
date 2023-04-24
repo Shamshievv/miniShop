66c53734df345b38aefdaafd1f3910576733bd2a
Azamat <azamatshamshiev18@gmail.com>
2023-04-24 15:13:32 +0300

    aza

--- src/component/Basket/index.js
+++ src/component/Basket/index.js
@@ -41,7 +41,7 @@ const Basket = () => {
                         Wallet
                         <div className="tooltip-arrow" data-popper-arrow></div>
                     </div>
-                    <div className="flex items-center justify-center">
+                    <Link to={"/product"} className="flex items-center justify-center">
                         <button data-tooltip-target="tooltip-new" type="button"
                                 className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
                             <svg className="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20"
@@ -51,7 +51,7 @@ const Basket = () => {
                             </svg>
                             <span className="sr-only">New item</span>
                         </button>
-                    </div>
+                    </Link>
                     <div id="tooltip-new" role="tooltip"
                          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                         Create new item
